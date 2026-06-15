778816df72b6411bd4fd8052e901e5381fc54019
circular <circular@circulars.dev>
2026-06-15 16:16:09 +1000

    test dice roll ui

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,59 @@
 mod dnd;
 
-use iced::widget::{button, text, column, Column};
+use iced::widget::{button, text, column, Column, text_input};
 
-use crate::dnd::*;
+use crate::dnd::dice::Dice;
 
 pub fn main() -> iced::Result {
-    iced::run(Counter::update, Counter::view)
+    iced::application(State::default, State::update, State::view).run()
 }
 
-#[derive(Default)]
-struct Counter {
-    value: i64,
+struct State {
+    sides_input: String,
+    amount_input: String,
+    dice_result: String,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 enum Message {
-    Increment,
-    Decrement,
+    SidesChanged(String),
+    AmountChanged(String),
+    DiceRolled,
 }
 
-impl Counter {
+impl State {
+    fn default() -> Self {
+        State {
+            sides_input: "".parse().unwrap(),
+            amount_input: "".parse().unwrap(),
+            dice_result: "".parse().unwrap(),
+        }
+    }
+
     fn update(&mut self, message: Message) {
         match message {
-            Message::Increment => {
-                self.value += 1;
-            }
-            Message::Decrement => {
-                self.value -= 1;
+            Message::SidesChanged(sides) => { self.sides_input = sides; }
+            Message::AmountChanged(amount) => { self.amount_input = amount; }
+            Message::DiceRolled => {
+                if let Ok(sides) = self.sides_input.parse::<u16>() && let Ok(amount) = self.amount_input.parse::<u16>() {
+                    let dice = Dice::new(sides, amount);
+                    self.dice_result = dice.roll().to_string();
+                } else {
+                    self.dice_result = "invalid".to_string();
+                }
             }
         }
     }
 
     fn view(&self) -> Column<'_, Message> {
-        let increment = button("+").on_press(Message::Increment);
-        let decrement = button("-").on_press(Message::Decrement);
+        let sides = text_input("sides", &self.sides_input).on_input(Message::SidesChanged);
+        let amount = text_input("amount", &self.amount_input).on_input(Message::AmountChanged);
+
+        let roll = button("roll").on_press(Message::DiceRolled);
         
-        let counter = text(self.value);
+        let result = text(&self.dice_result);
         
-        let interface = column![increment, counter, decrement];
+        let interface = column![sides, amount, roll, result];
         
         interface
     }
